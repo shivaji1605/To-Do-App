b78bd5d28a70d174e9a5563e1b77b10e965c26d7
shivaji1605 <shivaji160505@gmail.com>
2026-02-20 11:14:01 +0530

    sqflite added

---
+++ lib/todomodel.dart
@@ -0,0 +1,14 @@
+class model{
+  int id;
+  String title;
+  String decscription;
+  String date;
+
+  model({
+    required this.id,
+    required this.title,
+    required this.decscription,
+    required this.date,
+  });
+
+}
